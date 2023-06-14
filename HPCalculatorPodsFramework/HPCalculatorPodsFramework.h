ca58cd3313c9b6694565619955755914cebe4004
Harish <harish.nagarro@gmail.com>
2023-06-14 13:38:58 +0530

    Initial Commit

---
+++ HPCalculatorPodsFramework/HPCalculatorPodsFramework.h
@@ -0,0 +1,18 @@
+//
+//  HPCalculatorPodsFramework.h
+//  HPCalculatorPodsFramework
+//
+//  Created by Harish on 14/06/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HPCalculatorPodsFramework.
+FOUNDATION_EXPORT double HPCalculatorPodsFrameworkVersionNumber;
+
+//! Project version string for HPCalculatorPodsFramework.
+FOUNDATION_EXPORT const unsigned char HPCalculatorPodsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HPCalculatorPodsFramework/PublicHeader.h>
+
+
